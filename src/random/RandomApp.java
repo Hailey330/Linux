c4b63c1c7dbd1c1d6f4c29c8fc74533b97966193
baegopa <baegopa@wamwam.com>
2020-05-27 16:01:39 +0900

    Random

---
+++ src/random/RandomApp.java
@@ -0,0 +1,12 @@
+package random;
+
+import java.util.Random;
+
+public class RandomApp {
+
+	public static void main(String[] args) {
+		Random r = new Random();
+		int num = r.nextInt(50)+1;
+		System.out.println("random value : " + num);
+	}
+}
